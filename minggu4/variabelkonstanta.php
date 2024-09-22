d97fa84082ecea5df8d01e3a8fae9ad724a655a4
Rakai Seto <mamangrakai@gmail.com>
2024-09-22 21:41:50 +0700

    P4_Percobaan_1

---
+++ minggu4/variabelkonstanta.php
@@ -0,0 +1,13 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil dari $angka1 + $angka2 adalah $hasil.";
+$benar = true;
+$salah = false;
+echo "Variabel benar: $benar, variabel salah: $salah.";
+define("NAMA_SITUS", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2023);
+
+echo "SELAMAT DATANG DI " . NAMA_SITUS . " SITUS YANG DIDIRIKAN PADA TAHUN " . TAHUN_PENDIRIAN . ".";
+?>
