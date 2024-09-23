f94b04e3f68c4e7643419d6ba3f5a647f565cd64
Rakai Seto <mamangrakai@gmail.com>
2024-09-23 16:15:12 +0700

    P4_Percobaan_3

---
+++ minggu4/operator.php
@@ -0,0 +1,20 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurat = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Variabel a: {$a}<br>";
+echo "Variabel b: {$b}<br>";
+echo "Hasil dari $a + $b adalah <u>$hasilTambah</u>.<br>";
+echo "Hasil dari $a - $b adalah <u>$hasilKurat</u>.<br>";
+echo "Hasil dari $a * $b adalah <u>$hasilKali</u>.<br>";
+echo "Hasil dari $a / $b adalah <u>$hasilBagi</u>.<br>";
+echo "Hasil dari $a % $b adalah <u>$sisaBagi</u>.<br>";
+echo "Hasil dari $a ** $b adalah <u>$pangkat</u>.<br>";
+?>
