9de3cba9a20f56a1f8c03a879f276ad3cf83064b
Rakai Seto <mamangrakai@gmail.com>
2024-09-23 23:41:23 +0700

    P4_Percobaan_3_Pertanyaan_9

--- minggu4/operator.php
+++ minggu4/operator.php
@@ -17,4 +17,21 @@ echo "Hasil dari $a * $b adalah <u>$hasilKali</u>.<br>";
 echo "Hasil dari $a / $b adalah <u>$hasilBagi</u>.<br>";
 echo "Hasil dari $a % $b adalah <u>$sisaBagi</u>.<br>";
 echo "Hasil dari $a ** $b adalah <u>$pangkat</u>.<br>";
+
+$hasilSama = $a == $b;
+$hasiltidakSama = $a != $b;
+$hasillebihKecil = $a < $b;
+$hasillebihKecilSama = $a <= $b;
+$hasillebihBesar = $a > $b;
+$hasillebihBesarSama = $a >= $b;
+
+echo "<br>";
+echo "Variabel a: {$a}<br>";
+echo "Variabel b: {$b}<br>";
+echo "Variabel a sama dengan b: <u>$hasiltidakSama</u>.<br>";
+echo "Variabel a tidak sama dengan b: <u>$hasilSama</u>.<br>";
+echo "Variabel a lebih kecil dari b: <u>$hasillebihKecil</u>.<br>";
+echo "Variabel a lebih kecil dari sama dengan b: <u>$hasillebihKecilSama</u>.<br>";
+echo "Variabel a lebih besar dari b: <u>$hasillebihBesar</u>.<br>";
+echo "Variabel a lebih besar dari sama dengan b: <u>$hasillebihBesarSama</u>.<br>";
 ?>
