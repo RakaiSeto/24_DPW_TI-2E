a5c0679df168c255d733fb8b886306b72d110d47
Rakai Seto <mamangrakai@gmail.com>
2024-09-24 10:52:45 +0700

    P4_Percobaan_4_Pertanyaan_21

---
+++ minggu4/kontrol3.php
@@ -0,0 +1,11 @@
+<?php
+$angka = 661;
+
+echo "Angka: $angka <br>";
+
+if ($angka > 500) {
+    echo "Pemain mendapatkan hadiah tambahan";
+} else {
+    echo "Pemain tidak mendapatkan hadiah tambahan";
+}
+?>
