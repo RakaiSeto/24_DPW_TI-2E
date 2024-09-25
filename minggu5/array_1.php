bb055c0dbae7dfc1b571acf5d9e3839f4a16f29b
Rakai Seto <mamangrakai@gmail.com>
2024-09-25 20:22:25 +0700

    P5_Percobaan_1

---
+++ minggu5/array_1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Array Terindeks</title>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $ListDosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+        foreach ($ListDosen as $dosen) {
+            echo $dosen . "<br>";
+        }
+        ?>
+    </body>
+</html>
