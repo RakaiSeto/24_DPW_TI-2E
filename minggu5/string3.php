15965bf222ce5918eb9b649e341d4e2fdf50d5d9
Rakai Seto <mamangrakai@gmail.com>
2024-09-29 19:55:09 +0700

    P5_Pertanyaan_16

--- minggu5/string3.php
+++ minggu5/string3.php
@@ -1,4 +1,7 @@
 <?php
     $pesan = "Saya arek Malang";
-    echo strrev($pesan) . "<br>";
+    $pesanPerKata = explode(" ", $pesan);
+    $pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+    $pesan = implode(" ", $pesanPerKata);
+    echo $pesan;
 ?>
