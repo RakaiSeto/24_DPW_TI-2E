00c60be0f9a7de3d19ade2166920d2df0cea5e4a
Rakai Seto <mamangrakai@gmail.com>
2024-09-29 19:13:00 +0700

    P5_Pertanyaan_11

---
+++ minggu5/array_menu.php
@@ -0,0 +1,53 @@
+<?php
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
+
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}</li>";
+//        if (isset($item['subMenu'])) {
+//            echo "<ul>";
+//            foreach ($item['subMenu'] as $subItem) {
+//                echo "<li>{$subItem['nama']}</li>";
+//            }
+//            echo "</ul>";
+//        }
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
+
+?>
