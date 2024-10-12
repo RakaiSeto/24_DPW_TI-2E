6701dbf8d1dbc0a796bc48937cc588093359403c
Rakai Seto <mamangrakai@gmail.com>
2024-10-12 15:03:53 +0700

    P7_Pertanyaan_11

--- minggu7/regex.php
+++ minggu7/regex.php
@@ -1,19 +1,7 @@
 <?php
-$pattern = '/[a-z]/';
-$text = "This is a Sample text";
-
-if (preg_match($pattern, $text)) {
-    echo "Teks berisi huruf kecil";
-} else {
-    echo "Teks tidak berisi huruf";
-}
-
-$pattern = '/[0-9]+/';
-$text = "There are 123 Apples";
-
-if (preg_match($pattern, $text)) {
-    echo "Teks berisi angka";
-} else {
-    echo "Teks tidak berisi angka";
-}
+$pattern = '/apple/';
+$replacement = 'banana';
+$string = 'I like apple pie';
+$new_text = preg_replace($pattern, $replacement, $string);
+echo $new_text;
 ?>
