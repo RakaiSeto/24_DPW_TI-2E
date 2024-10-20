4d82abc953205fba15dde2e6a4836b3292dcd862
Rakai Seto <mamangrakai@gmail.com>
2024-10-20 23:03:42 +0700

    P8_Pertanyaan_3

--- minggu8/upload.php
+++ minggu8/upload.php
@@ -2,11 +2,19 @@
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/";
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-        echo "File berhasil diupload";
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxSize = 1024 * 1024 * 5; // 2 MB
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] < $maxSize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diupload";
+        } else {
+            echo "Gagal mengupload file";
+        }
     } else {
-        echo "Gagal mengupload file";
+        echo "Tipe file tidak diizinkan atau ukuran file terlalu besar";
     }
 } else {
     echo "tidak muncul apa2";
