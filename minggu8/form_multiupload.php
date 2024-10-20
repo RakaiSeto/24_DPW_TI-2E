167d52f15eaa0be3abfc11173c4d734cd9e48003
Rakai Seto <mamangrakai@gmail.com>
2024-10-20 23:38:21 +0700

    P8_Pertanyaan_9

--- minggu8/form_multiupload.php
+++ minggu8/form_multiupload.php
@@ -5,7 +5,7 @@
     <body>
         <h2>Upload File</h2>
         <form method="post" action="proses_upload.php" enctype="multipart/form-data">
-            <input type="file" multiple name="files[]" accept=".pdf, .doc, .docx">
+            <input type="file" multiple name="files[]" accept=".img, .png, .jpg">
             <input type="submit" value="Unggah">
         </form>
     </body>
