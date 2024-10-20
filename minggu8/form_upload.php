1fa39301ac8ae3986b26a26ff8d3fe2faa3027cd
Rakai Seto <mamangrakai@gmail.com>
2024-10-20 22:28:30 +0700

    P8_Pertanyaan_1

---
+++ minggu8/form_upload.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Upload File</title>
+</head>
+<body>
+    <h2>Upload File</h2>
+    <form method="post" action="upload.php" enctype="multipart/form-data">
+        <input type="file" name="myfile">
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
