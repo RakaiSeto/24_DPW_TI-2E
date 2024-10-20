60e0eac95535336ad41a7d761c4db429e6dd3928
Rakai Seto <mamangrakai@gmail.com>
2024-10-20 23:10:55 +0700

    P8_Pertanyaan_5

--- minggu8/upload.php
+++ minggu8/upload.php
@@ -10,6 +10,7 @@ if (isset($_POST["submit"])) {
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] < $maxSize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diupload";
+            echo "<img src='" . $targetfile . "' width='200' style='height: auto;'>";
         } else {
             echo "Gagal mengupload file";
         }
