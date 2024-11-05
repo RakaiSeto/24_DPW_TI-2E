f519070811e55933d1c3b968b10f3068e0571ce3
Rakai Seto <mamangrakai@gmail.com>
2024-11-05 11:07:06 +0700

    P10_Pertanyaan_3

--- minggu10/proses.php
+++ minggu10/proses.php
@@ -15,4 +15,21 @@ if ($aksi == "tambah") {
     } else {
         echo "Data gagal disimpan";
     }
+} elseif ($aksi == "ubah") {
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+
+        $query = "UPDATE anggota SET nama = '$nama', jenis_kelamin = '$jenis_kelamin', alamat = '$alamat', no_telp = '$no_telp' WHERE id = '$id'";
+
+        if (mysqli_query($koneksi, $query)) {
+            header("location:index.php");
+            exit();
+        } else {
+            echo "Gagal mengupdate data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid";
+    }
 }
+
+mysqli_close($koneksi);
