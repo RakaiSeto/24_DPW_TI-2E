5ac744f70b5ffab8d5bc667c743c272dcc365602
Rakai Seto <mamangrakai@gmail.com>
2025-01-01 22:39:30 +0700

    P12_Pertanyaan_1,7

--- minggu12/interface.php
+++ minggu12/interface.php
@@ -1,27 +1,33 @@
 <?php
 
-interface Shape {
+interface Shape
+{
     public function calculateArea();
 }
 
-interface Color {
+interface Color
+{
     public function getColor();
 }
 
-class Circle implements Shape, Color {
+class Circle implements Shape, Color
+{
     private $radius;
     private $color;
 
-    public function __construct($radius, $color) {
+    public function __construct($radius, $color)
+    {
         $this->radius = $radius;
         $this->color = $color;
     }
 
-    public function calculateArea() {
+    public function calculateArea()
+    {
         return pi() * pow($this->radius, 2);
     }
 
-    public function getColor() {
+    public function getColor()
+    {
         return $this->color;
     }
 }
